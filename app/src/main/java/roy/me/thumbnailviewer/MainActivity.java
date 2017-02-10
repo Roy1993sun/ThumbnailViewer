4150c1aa9f30123ef4f6b37f7ef66028bae83e79
AwesomeRoy <roy1993sun@gmail.com>
2017-02-10 22:32:05 +0800

    实现读取相册和视频并加载到首页上。

--- app/src/main/java/roy/me/thumbnailviewer/MainActivity.java
+++ app/src/main/java/roy/me/thumbnailviewer/MainActivity.java
@@ -2,32 +2,51 @@ package roy.me.thumbnailviewer;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Build;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.app.LoaderManager;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.orhanobut.logger.Logger;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     public static final int READ_EXTERNAL_STORAGE_CODE = 0;
+    public static final int MEDIA_STORE_LOADER_ID = 0;
+
+    private RecyclerView mThumbnailRecyclerView;
+    private MediaStoreAdapter mMediaStoreAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         checkReadExternalStoragePermission();
+
+        mThumbnailRecyclerView = (RecyclerView) findViewById(R.id.thumbnail_RecyclerView);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
+        mThumbnailRecyclerView.setLayoutManager(gridLayoutManager);
+        mMediaStoreAdapter = new MediaStoreAdapter(this);
+        mThumbnailRecyclerView.setAdapter(mMediaStoreAdapter);
+
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
         switch (requestCode) {
             case READ_EXTERNAL_STORAGE_CODE:
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // TODO: 2017/2/10 call cursor loader
-                    Toast.makeText(this, "已获得权限", Toast.LENGTH_SHORT).show();
+                    getSupportLoaderManager().initLoader(MEDIA_STORE_LOADER_ID, null, this);
                 }
                 break;
 
@@ -39,16 +58,43 @@ public class MainActivity extends AppCompatActivity {
 
     private void checkReadExternalStoragePermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                // TODO: 2017/2/10 读取内容
+            if (ContextCompat.checkSelfPermission(this,
+                                                  Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                getSupportLoaderManager().initLoader(MEDIA_STORE_LOADER_ID, null, this);
             } else {
-                if (shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                if (shouldShowRequestPermissionRationale(
+                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
                     Toast.makeText(this, "需要读取相册的权限", Toast.LENGTH_SHORT).show();
                 }
-                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, READ_EXTERNAL_STORAGE_CODE);
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                   READ_EXTERNAL_STORAGE_CODE);
             }
         } else {
-            // TODO: 2017/2/10 直接读取内容
+            getSupportLoaderManager().initLoader(MEDIA_STORE_LOADER_ID, null, this);
+
         }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String[] projection = {MediaStore.Files.FileColumns._ID,
+                               MediaStore.Files.FileColumns.DATE_ADDED,
+                               MediaStore.Files.FileColumns.MEDIA_TYPE,};
+        String selection = MediaStore.Files.FileColumns.MEDIA_TYPE + "="
+                + MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE + " OR "
+                + MediaStore.Files.FileColumns.MEDIA_TYPE + "="
+                + MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
+        Logger.d(selection);
+        return new CursorLoader(this,MediaStore.Files.getContentUri("external"),projection,selection,null,MediaStore.Files.FileColumns.DATE_ADDED + " DESC");
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mMediaStoreAdapter.changeCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mMediaStoreAdapter.changeCursor(null);
     }
 }
---
+++ app/src/main/java/roy/me/thumbnailviewer/MediaStoreAdapter.java
@@ -0,0 +1,104 @@
+package roy.me.thumbnailviewer;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * Created by Roy Sun on 2017/2/10.
+ */
+
+public class MediaStoreAdapter extends RecyclerView.Adapter<MediaStoreAdapter.ViewHolder> {
+
+    private       Cursor   mMediaStoreCursor;
+    private final Activity mActivity;
+
+    public MediaStoreAdapter(Activity activity) {
+        mActivity = activity;
+    }
+
+    @Override
+    public MediaStoreAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                                  .inflate(R.layout.media_image_view, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Bitmap bitmap = getBitmapFromMediaStore(position);
+        if (bitmap != null) {
+            holder.getImageView().setImageBitmap(bitmap);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return (mMediaStoreCursor == null) ? 0 : mMediaStoreCursor.getCount();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final ImageView mImageView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            mImageView = (ImageView) itemView.findViewById(R.id.media_store_imageView);
+        }
+
+        public ImageView getImageView() {
+            return mImageView;
+        }
+    }
+
+    private Cursor swapCursor(Cursor cursor) {
+        if (cursor == mMediaStoreCursor) {
+            return null;
+        }
+        Cursor oldCursor = mMediaStoreCursor;
+        this.mMediaStoreCursor = cursor;
+        if (cursor != null) {
+            this.notifyDataSetChanged();
+        }
+        return oldCursor;
+    }
+
+    public void changeCursor(Cursor cursor) {
+        Cursor oldCursor = swapCursor(cursor);
+        if (oldCursor != null) {
+            oldCursor.close();
+        }
+    }
+
+    private Bitmap getBitmapFromMediaStore(int position) {
+        int idIndex = mMediaStoreCursor.getColumnIndex(MediaStore.Files.FileColumns._ID);
+        int mediaTypeIndex = mMediaStoreCursor.getColumnIndex(
+                MediaStore.Files.FileColumns.MEDIA_TYPE);
+
+        mMediaStoreCursor.moveToPosition(position);
+        switch (mMediaStoreCursor.getInt(mediaTypeIndex)) {
+            case MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE:
+                return MediaStore.Images.Thumbnails.getThumbnail(mActivity.getContentResolver(),
+                                                                 mMediaStoreCursor.getLong(idIndex),
+                                                                 MediaStore.Images.Thumbnails.MICRO_KIND,
+                                                                 null);
+            case MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO:
+                return MediaStore.Video.Thumbnails.getThumbnail(mActivity.getContentResolver(),
+                                                                mMediaStoreCursor.getLong(idIndex),
+                                                                MediaStore.Video.Thumbnails.MICRO_KIND,
+                                                                null);
+
+            default:
+                return null;
+        }
+
+    }
+}
