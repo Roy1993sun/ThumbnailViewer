825f2005b74c9156f23483405761b15bfcd08664
AwesomeRoy <roy1993sun@gmail.com>
2017-02-15 14:07:42 +0800

    使用ConstraintLayout构建Video Play Activity

---
+++ app/src/main/java/roy/me/thumbnailviewer/VideoPlayActivity.java
@@ -0,0 +1,13 @@
+package roy.me.thumbnailviewer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VideoPlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vedio_play);
+    }
+}
